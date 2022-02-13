🐛 Fix off-by-one-error when selecting best clustering

pydiar/models/binary_key/__init__.py
@@ -178,7 +178,7 @@ class BinaryKeyDiarizationModel(DiarizationModel):
             k,
             self.CLUSTERING_SELECTION_MAX_SPEAKERS,
         ).astype(int)
-        best_clustering = finalClusteringTable[:, bestClusteringID - 1]
+        best_clustering = finalClusteringTable[:, bestClusteringID]
         logging.info(
             f"Best: {bestClusteringID} with "
             f"{np.size(np.unique(best_clustering), 0)} clusters"

pydiar/models/binary_key/diarizationFunctions.py
@@ -443,13 +443,15 @@ def getBestClustering(
     vecToLine = vecFromFirst - vecFromFirstParallel
     distToLine = np.sqrt(np.sum(np.square(vecToLine), axis=1))
     bestClusteringID = allCoord[np.argmax(distToLine)][0]
-
+    print(allCoord, distToLine, bestClusteringID)
     # Select best clustering that matches max speaker limit
     nrSpeakersPerSolution = np.zeros((clusteringTable.shape[1]))
     for k in np.arange(clusteringTable.shape[1]):
         nrSpeakersPerSolution[k] = np.size(np.unique(clusteringTable[:, k]))
 
     firstAllowedClustering = np.min(np.where(nrSpeakersPerSolution <= maxNrSpeakers))
+    print(f"{nrSpeakersPerSolution=}")
+    print(f"{firstAllowedClustering=}")
     # Note: clusters are ordered from most clusters to least, so this selects the bestClusteringID
     # unless it has more than maxNrSpeakers nodes, in which case it selects firstAllowedClustering
     bestClusteringID = np.maximum(